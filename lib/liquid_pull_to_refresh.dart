b35af9c271cc71ca49a3d4b22329a13fa9132e9a
Debasis Koley <debasis.koley@itobuz.com>
2019-06-25 17:41:46 +0530

    static padding added

--- lib/liquid_pull_to_refresh.dart
+++ lib/liquid_pull_to_refresh.dart
@@ -711,6 +711,7 @@ class _LiquidPullToRefreshState extends State<LiquidPullToRefresh>
                     : 0.0,
               ),
               child: Container(
+                padding: EdgeInsets.only(top:0.0),
                 height: _value.value * height * 2, // 100.0
                 // color: color,
                 decoration: decoration,
